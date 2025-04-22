854264e8a8be3181628b3c00836583d2a9040a44
dacaniulao23 <da.caniulao@duocuc.cl>
2025-04-22 16:25:44 -0400

    for exercise

---
+++ for.py
@@ -0,0 +1,53 @@
+# #ingreso de variables para concatenar
+# print ("ingrese su nombre")
+# nombre=imput
+# print("ingrese su edad")
+# edad=imput
+
+# #ejemplo de concatenacion
+# print("hello, ", nombre, ", your age is ", edad)
+
+# #suma de 2 numeros
+# n1=int(input("ingrese un número"))
+# n2=int(input("ingrese un número"))
+
+# print
+
+
+# for i in range (1,6):
+#         print ("repeticion", i+1)
+# cant=int(input("ingrese un número)"))
+
+# nombre=input("ingrese su nombre")
+# edad=input("ingrese su edad")
+
+# print("hola, ", nombre, ", su edad es ", edad)
+
+# num=int(input("ingrese un número"))
+# for i in range ( 1,11):
+#         print("esta es la tabla del")
+#         for i in range(1,11):
+#                 print
+
+
+# cant=int(input("ingrese la cantidad de notas"))
+# total=0
+# for i in range(cant):
+#     print("ingrese la nota", i+1)
+#     nota=float(input())
+#     total=total+nota
+# prom=total/cant
+
+
+# print("Su promedio es ", prom)
+
+
+clave=1234
+for i in range (3):
+    password=int(input("ingrese la password :"))
+
+    if password==clave:
+        print("ingrese al sistema")
+        break
+    else:
+        print("clave incorrecta")

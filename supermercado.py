1ec2bc8fadfec0d90569527f41459ce6f9d9d538
dacaniulao23 <da.caniulao@duocuc.cl>
2025-04-22 16:42:26 -0400

    supermercado tarea

---
+++ supermercado.py
@@ -0,0 +1,13 @@
+#haremos el intento
+
+cant=int(input("¿cuantos productos va a llevar?"))
+prod=int(input("¿qué productos va a llevar?"
+                "1-juguete"
+                "2-bebida"
+                "3-cartulina"))
+for i in range(cant):
+    if prod==1:
+        print("el precio es $450")
+    elif prod==2:
+        print ("el producto es $120")
+    

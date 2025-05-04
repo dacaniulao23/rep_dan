a964d8c37c585858a3bc7f4afca338911bdb8616
dacaniulao23 <da.caniulao@duocuc.cl>
2025-05-04 16:36:54 -0400

    new file:   practice.py 	practica.py

---
+++ practice.py
@@ -0,0 +1,83 @@
+# #print = escribir
+print("es hora de practicar")
+
+# #input = leer
+# #int = definir como ENTERO
+n1=int(input("ingrese un número"))
+
+r1=0
+r2=0
+r3=0
+
+# #for __ in range() = para __ hasta __
+for step in range(n1):
+    n2=int(input("ingrese otro número"))
+#     #if = si __ entonces
+#     #and = y
+    if n2>=0 and n2<4:
+        r1=r1+1
+        print(r1)
+#     #elif = sino
+    elif n2>=4 and n2<8:
+        r2=r2+1
+        print(r2)
+#     #or = o
+    elif n2==7 or n2<10:
+        r3=r3+1
+        print(r3)
+#     #elif = sino "final"
+    else:
+        print("error")
+
+if r1>r2 and r1>r3:
+    print ("el mayor alcanzó a ser ", r1)
+elif r1<r2 and r2>r3:
+    print ("el mayor alcanzó a ser ", r2)
+else:
+    print ("el mayor alcanzó a ser ", r3)
+
+#str = definir como CARACTER
+contraseña="torna"
+clave=str(input("ingrese clave"))
+while clave!=contraseña:
+    print("clave incorrecta, intente de nuevo")
+    clave=input()
+
+n3=int(input("ingrese un nuevo número"))
+#import = insertar comando
+import time
+par=0
+impar=0
+total=0
+for pas in range(1,n3+1):
+#incluir (1,__+1) para que el conteo NO SEA desde 0
+#y que incluya el número límite
+    print(pas)
+    #time.sleep(_) = esperar __ segundos/minutos
+    time.sleep(1)
+    # % = MOD
+    if pas%2==0:
+        par=par+1
+        total=total+pas
+    else:
+        impar=impar+1
+        total=total+pas
+print("el total de pares fue ", par)
+print("el total de impares fue", impar)
+print("la suma total es ", total)
+
+#from random = aleatorio
+import random
+#randint(_) = aleatorio(_)
+azar1=random.randint(1,5)
+azar2=random.randint(6,10)
+print("resultado aleatorio de ", azar1)
+print("resultado aleatorio de ", azar2)
+
+#import random
+#___=random.randint()
+
+
+
+#from random import randint
+#___=randint()
